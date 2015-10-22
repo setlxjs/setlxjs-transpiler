05f3354eb3bd4db42c5c3b57d17ae47fc228bbff
Hendrik Niemann <h.neverman@web.de>
2015-10-22 19:48:45 +0200

    enable babel and eslint

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,24 @@
 const gulp = require('gulp');
 const peg = require('gulp-peg');
 const mocha = require('gulp-mocha');
+const babel = require('gulp-babel');
 
-gulp.task('default', () => {
+gulp.task('make:grammar', () => {
   gulp.src('src/grammar/*.pegjs')
     .pipe(peg())
     .pipe(gulp.dest('build/'));
 });
+
+gulp.task('make:helpers', () => {
+  gulp.src('src/helper/**/*.js')
+    .pipe(babel())
+    .pipe(gulp.dest('build/'));
+});
+
+gulp.task('default', [
+  'make:grammar',
+  'make:helpers',
+]);
 
 gulp.task('test:grammar', () => {
   gulp.src('test/grammar/*.test.js')
