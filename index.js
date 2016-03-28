ddef117bc38c90345c425ab7337f7978bae59cfc
Hendrik Niemann <h.neverman@web.de>
2016-03-28 16:21:10 +0200

    make package requireable

---
+++ index.js
@@ -0,0 +1,38 @@
+var parser = require('build/parse');
+var transpiler = require('build/index');
+
+module.exports = function transpiler(input, options, cb) {
+  var callback, opts, promise, out;
+  if (typeof options === 'function') {
+    callback = options;
+  } else if (typeof options === 'undefined') {
+    opts = {};
+  } else {
+    opts = options;
+    callback = cb;
+  }
+
+  if (opts.tree) {
+    try {
+      out = parser(input);
+      promise = new Promise.resolve(out);
+    } catch (error) {
+      promise = Promise.reject(error);
+    }
+  } else {
+    try {
+      out = transpiler(input);
+      promise = new Promise.resolve(out);
+    } catch (error) {
+      promise = Promise.reject(error);
+    }
+  }
+
+  if (typeof callback === 'function') {
+    promise
+      .then(function(output) { callback(null, output); })
+      .catch(callback);
+    return null;
+  }
+  return promise;
+};
