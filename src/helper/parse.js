f9f77b936ed3a098681ba44065427de0e034209a
Hendrik Niemann <h.neverman@web.de>
2016-03-30 22:11:09 +0200

    enhance parsers SyntaxErrors with line and column
    SyntaxError contains a line and column property but does not display them
    in the message. The parser now catches SyntaxErrors and adds the line and
    column to the error message.

--- src/helper/parse.js
+++ src/helper/parse.js
@@ -1,5 +1,14 @@
 import { parse } from './grammar/grammar';
 
 export default function parser( fileContent ) {
-  return parse( fileContent );
+  try {
+    return parse( fileContent );
+  } catch (error) {
+    if (error.name === 'SyntaxError') {
+      error.message = error.message.replace(/\.$/, '') +
+        ` in line ${ error.line }:${ error.column }.`;
+      throw error;
+    }
+    throw error;
+  }
 }
