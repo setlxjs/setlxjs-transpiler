8abf11ae803005392a83fc2e0559c99a3c125de2
Hendrik Niemann <h.neverman@web.de>
2016-04-01 14:04:45 +0200

    add if statement tests

---
+++ test/syntaxtree/ifstmt.test.js
@@ -0,0 +1,60 @@
+require('should');
+const Block = require('../../build/classes/Block');
+const Statement = require('../../build/classes/Statement');
+const Identifier = require('../../build/classes/Identifier');
+const Primitive = require('../../build/classes/Primitive');
+const Assignment = require('../../build/classes/Assignment');
+const Comparison = require('../../build/classes/Comparison');
+const IfStmt = require('../../build/classes/IfStmt');
+
+const types = require('../../build/constants/types');
+const ops = require('../../build/constants/operators');
+
+const parser = require('../../build/parse');
+
+function makeStmt(expr) {
+  return Block([Statement(expr)]);
+}
+
+describe('IfStmt', () => {
+  it('should parse if statements without else', () => {
+    parser('if(true) { x := 1; }').should.eql(Block([
+      IfStmt(
+        Primitive(types.BOOLEAN, true),
+        makeStmt(Assignment(Identifier('x'), Primitive(types.INTEGER, 1)))
+      ),
+    ]));
+  });
+
+  it('should parse if statements with else', () => {
+    parser('if(x == 1) { x := 4; } else { x := 5; }').should.eql(Block([
+      IfStmt(
+        Comparison(ops.EQUAL, Identifier('x'), Primitive(types.INTEGER, 1)),
+        makeStmt(Assignment(Identifier('x'), Primitive(types.INTEGER, 4))),
+        makeStmt(Assignment(Identifier('x'), Primitive(types.INTEGER, 5)))
+      ),
+    ]));
+  });
+
+  it('should parse if statements with else if and else', () => {
+    parser(`
+      if(x == 2) {
+        x := 6;
+      } else if (x == 1) {
+        x := 4;
+      } else {
+        x := 5;
+      }
+    `).should.eql(Block([
+      IfStmt(
+        Comparison(ops.EQUAL, Identifier('x'), Primitive(types.INTEGER, 2)),
+        makeStmt(Assignment(Identifier('x'), Primitive(types.INTEGER, 6))),
+        IfStmt(
+          Comparison(ops.EQUAL, Identifier('x'), Primitive(types.INTEGER, 1)),
+          makeStmt(Assignment(Identifier('x'), Primitive(types.INTEGER, 4))),
+          makeStmt(Assignment(Identifier('x'), Primitive(types.INTEGER, 5)))
+        )
+      ),
+    ]));
+  });
+});
