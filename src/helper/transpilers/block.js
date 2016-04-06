ace4b2bca9056a63c2067b673db9de8558fc5806
hendrikniemann <h.neverman@web.de>
2016-04-07 00:50:09 +0200

    add empty block edge case

--- src/helper/transpilers/block.js
+++ src/helper/transpilers/block.js
@@ -1,3 +1,6 @@
 export default function block(tree, transpile) {
+  if (tree.statements.length === 0) {
+    return '';
+  }
   return tree.statements.map(transpile).join('\n') + '\n';
 }
--- test/transpilers/block.test.js
+++ test/transpilers/block.test.js
@@ -6,6 +6,10 @@ const Statement = require('../../build/classes/Statement');
 const Block = require('../../build/classes/Block');
 
 describe('transpilers/block', () => {
+  it('should transpile empty blocks', () => {
+    transpile(Block([])).should.be.exactly('');
+  });
+
   it('should transpile statements seperated with newlines', () => {
     transpile(Block([
       Statement(Identifier('idone')),
