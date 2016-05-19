950e33667fe746cad2e93e3de42848c097f59a28
hendrikniemann <h.neverman@web.de>
2016-05-19 16:50:53 +0200

    handle list assignments
    See #5

--- src/transpilers/assignment.js
+++ src/transpilers/assignment.js
@@ -1,3 +1,14 @@
-export default function assignment(tree, transpile) {
-  return transpile(tree.receiver) + ' = ' + transpile(tree.expression);
+import { CALL, ASSIGNABLE_LIST } from '../constants/tokens';
+
+export default function assignment({ receiver, expression }, transpile) {
+  if (receiver.token === CALL) {
+    const realReceiver = transpile(receiver.receiver);
+    const accessor = transpile(receiver.call.accessor);
+
+    return `${realReceiver} = ${realReceiver}.set(${accessor}, ${transpile(expression)})`;
+  }
+  if (receiver.token === ASSIGNABLE_LIST) {
+    return transpile(receiver) + ' = ' + transpile(expression) + '.toArray()';
+  }
+  return transpile(receiver) + ' = ' + transpile(expression);
 }
--- test/transpilers/assignment.test.js
+++ test/transpilers/assignment.test.js
@@ -5,6 +5,8 @@ const Identifier = require('../../build/classes/Identifier');
 const Primitive = require('../../build/classes/Primitive');
 const Assignment = require('../../build/classes/Assignment');
 const AssignableList = require('../../build/classes/AssignableList');
+const Call = require('../../build/classes/Call');
+const CollectionAccess = require('../../build/classes/CollectionAccess');
 
 const types = require('../../build/constants/types');
 
@@ -18,6 +20,15 @@ describe('transpilers/assignment', () => {
   it('should transpile destructuring assignments correctly', () => {
     transpile(
       Assignment(AssignableList([Identifier('x'), Identifier('y')]), Identifier('mylist'))
-    ).should.be.exactly('[x, y] = mylist');
+    ).should.be.exactly('[x, y] = mylist.toArray()');
+  });
+
+  it('should transpile list elements assignments correctly', () => {
+    transpile(
+      Assignment(
+        Call(Identifier('mylist'), CollectionAccess(Primitive(types.INTEGER, 1))),
+        Primitive(types.INTEGER, 2)
+      )
+    ).should.be.exactly('mylist = mylist.set(1, 2)');
   });
 });
