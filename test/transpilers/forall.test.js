20ee5e7e4c1717c2e45420c1926e34b435278491
hendrikniemann <h.neverman@web.de>
2016-05-19 20:03:08 +0200

    cleanup forall test

--- test/transpilers/forall.test.js
+++ test/transpilers/forall.test.js
@@ -2,19 +2,13 @@ require('should');
 
 const transpile = require('../../build/transpile')();
 const Identifier = require('../../build/classes/Identifier');
-const Statement = require('../../build/classes/Statement');
-const Block = require('../../build/classes/Block');
 const Primitive = require('../../build/classes/Primitive');
 const Forall = require('../../build/classes/Forall');
 const Iterator = require('../../build/classes/Iterator');
 
 const types = require('../../build/constants/types');
 
-function makeStmt(expr) {
-  return Block([Statement(expr)]);
-}
-
-describe('transpilers/forLoop', () => {
+describe('transpilers/forall', () => {
   it('should transpile a foralls with one iterator', () => {
     transpile(
       Forall(
