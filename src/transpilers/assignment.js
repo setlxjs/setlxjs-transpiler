0467696bfb7de596847ada4e2e0cf353db3300ae
hendrikniemann <h.neverman@web.de>
2016-07-23 00:02:48 +0200

    add function hoisting
    Functions now don't have to appear in order of usage.

--- src/transpilers/assignment.js
+++ src/transpilers/assignment.js
@@ -1,6 +1,7 @@
-import { CALL, ASSIGNABLE_LIST } from '../constants/tokens';
+import { CALL, ASSIGNABLE_LIST, PROCEDURE } from '../constants/tokens';
 
 export default function assignment({ receiver, expression }, transpile) {
+  // lists have to be assigned with .set because immutable.js
   if (receiver.token === CALL) {
     const realReceiver = transpile(receiver.receiver);
     const accessor = transpile(receiver.call.accessor);
@@ -9,6 +10,12 @@ export default function assignment({ receiver, expression }, transpile) {
   }
   if (receiver.token === ASSIGNABLE_LIST) {
     return transpile(receiver) + ' = ' + transpile(expression) + '.toArray()';
+  }
+  if (expression.token === PROCEDURE) {
+    // this is a bit hacky, we remove the "function" of the declarion to insert the identifier
+    // in between...
+    const proc = transpile(expression).substr(8);
+    return 'function ' + transpile(receiver) + proc;
   }
   return transpile(receiver) + ' = ' + transpile(expression);
 }
